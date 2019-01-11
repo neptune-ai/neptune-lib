f8f8281fee76bf0f69b71bc9f70f46a553a300f4
jakubczakon <jakub.czakon@neptune.ml>
2019-01-11 20:36:35 +0100

    added project progress viz

--- neptunelib/contrib/visualizations.py
+++ neptunelib/contrib/visualizations.py
@@ -41,6 +41,43 @@ def combine_channel_df(experiments, channel_name):
     combined_df = pd.concat(combined_df, axis=0)
     return combined_df
 
+
+def create_progress_df(leaderbaord, metric_name):
+    system_columns = ['owner','running_time','finished', 'tags']
+    progress_columns = system_columns + [metric_name]
+    progress_df = leaderbaord[progress_columns]
+    
+    progress_df['finished'] = pd.to_datetime(progress_df['finished'])
+
+    progress_df[metric_name] = progress_df[metric_name].astype(float)
+
+    progress_df.sort_values('finished',inplace=True)
+    progress_df[metric_name] = progress_df[metric_name].fillna(method='bfill')
+    progress_df.dropna(subset=[metric_name], how='all', inplace = True)
+    
+    current_best = progress_df[metric_name].cummax()
+    current_best = current_best.fillna(method='bfill')
+    progress_df['{}_current_best'.format(metric_name)] = current_best
+        
+    progress_df = progress_df.melt(id_vars=system_columns,
+                        value_vars=[metric_name, '{}_current_best'.format(metric_name)],
+                        var_name='best_or_actual',
+                        value_name=metric_name
+                       )
+        
+    progress_df['running_time'] = np.round(progress_df['running_time'] / (60 * 60 * 24), 2)
+    progress_df['finished_date'] = [d.date() for d in progress_df['finished']]
+    daily_counts = progress_df.groupby('finished_date').count()['finished'].reset_index()
+    daily_counts.columns = ['finished_date','daily_experiment_counts']
+    progress_df = pd.merge(progress_df, daily_counts, on='finished_date')
+
+    progress_df['tags'] = progress_df['tags'].apply(lambda x: ' | '.join(x))
+    
+    progress_df['finished'] = progress_df['finished'].astype(str)
+    progress_df['finished_date'] = progress_df['finished_date'].astype(str)
+    return progress_df
+
+
 def curve_compare(experiments, channel_name):
     fig = plt.figure(figsize=(16,12))
     for experiment in experiments:
@@ -84,7 +121,6 @@ def interacvite_curve_compare(experiments, channel_name):
         interval
     )
 
-
     line = alt.Chart().mark_line().encode(
         x=alt.X('x:Q',title='iteration'),
         y=alt.Y('y:Q', scale=alt.Scale(zero=False), title=channel_name),
@@ -117,6 +153,105 @@ def interacvite_curve_compare(experiments, channel_name):
     combined = alt.hconcat(alt.vconcat(top_view, bottom_view),
                            legend, 
                            data=combined_df)
+    return combined
+
+
+def interactive_project_progress(leaderboard, metric_name):
+    nearest = alt.selection(type='single', nearest=True, on='mouseover',
+                            fields=['finished'], empty='none')
+
+    brush = alt.selection(type='interval', encodings=['x'])
+
+    top_view = alt.Chart(height=50, width=800).mark_line(interpolate='step-after', size=5).encode(
+        x='finished:T',
+        y=alt.Y('{}:Q'.format(metric_name),scale=alt.Scale(zero=False), axis=None),
+        color='best_or_actual:N'
+    ).add_selection(
+        brush
+    )
+
+    selectors = alt.Chart().mark_point().encode(
+        x=alt.X('finished:T', scale=alt.Scale()),
+        opacity=alt.value(0),
+    ).add_selection(
+        nearest
+    ).transform_filter(
+        brush
+    )
+
+    line = alt.Chart().mark_line(interpolate='step-after', size=5).encode(
+        x=alt.X('finished:T', scale=alt.Scale()),
+        y=alt.Y('{}:Q'.format(metric_name),scale=alt.Scale(zero=False)),
+        color='best_or_actual:N'
+    ).transform_filter(
+        brush
+    )
+
+    points = line.mark_point().encode(
+        opacity=alt.condition(nearest, alt.value(1), alt.value(0))
+    )
+
+    text = line.mark_text(align='left', dx=5, dy=-5, size=15).encode(
+        text=alt.condition(nearest, '{}:Q'.format(metric_name), alt.value(' ')),
+        color='best_or_actual:N'
+    )
+
+    rules = alt.Chart().mark_rule(color='gray').encode(
+        x=alt.X('finished:T', scale=alt.Scale()),
+    ).transform_filter(
+        nearest
+    )
+
+    metrics = alt.layer(line, points, text, rules, selectors).properties(
+        height=400,
+        width=800,
+    )
+
+
+    exp_line = alt.Chart().mark_area(interpolate='step-after', size=5).encode(
+        x=alt.X('finished:T', scale=alt.Scale()),
+        y=alt.Y('daily_experiment_counts:Q',scale=alt.Scale(zero=False)),
+        color=alt.ColorValue('pink'),
+        opacity=alt.OpacityValue(0.5)
+    ).transform_filter(
+        brush
+    )
+
+    exp_points = exp_line.mark_point(filled=True).encode(
+        color=alt.ColorValue('black'),
+        opacity=alt.condition(nearest, alt.value(1), alt.value(0))
+    )
+
+    exp_text = exp_line.mark_text(align='left', dx=5, dy=-5, fontWeight='bold', size=15).encode(
+        text=alt.condition(nearest, 'daily_experiment_counts:Q'.format(metric_name), alt.value(' ')),
+        color=alt.ColorValue('black')
+    )
+
+    exp_rules = alt.Chart().mark_rule(color='gray').encode(
+        x=alt.X('finished:T', scale=alt.Scale()),
+    ).transform_filter(
+        nearest
+    )
+
+    exps = alt.layer(exp_line, exp_points, exp_rules, exp_text).properties(
+        height=400,
+        width=800,
+    )
+
+    main_view = alt.layer(metrics, exps).properties(
+        height=400,
+        width=800,
+    ).resolve_scale(
+        y='independent'
+    )
+
+    tags = alt.Chart(height=1, width=1).mark_text(align='left', size=15, fontWeight='bold').encode(
+            x=alt.X('finished:T', axis=None),
+            text=alt.condition(nearest, 'tags:N', alt.value(' ')),
+    )
+
+    combined = alt.vconcat(top_view, tags, main_view,
+                           data=progress_df)
     return combined
 
 
