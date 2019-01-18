88a629155f6c819a82654e7ef0e1b183397f812c
kamil-kaczmarek <kamil.kaczmarek@neptune.ml>
2019-01-18 16:20:41 +0100

    RtD setup

--- docs/conf.py
+++ docs/conf.py
@@ -84,7 +84,11 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'vcs_pageview_mode': 'blob'
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
