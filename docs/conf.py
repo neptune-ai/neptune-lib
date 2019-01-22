6f2bb73fab8808562f8a24b36b416e0f2d0302d6
jakubczakon <jakub.czakon@neptune.ml>
2019-01-22 17:05:43 +0100

    updated version in docs

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Neptune Dev Team'
 author = 'Neptune Dev Team'
 
 # The short X.Y version
-version = '0.0'
+version = '0.1'
 # The full version, including alpha/beta/rc tags
-release = '0.0.3'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
